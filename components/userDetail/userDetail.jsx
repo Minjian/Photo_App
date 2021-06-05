2059a6df8f1de8a4b4dca1d8b6ad079644d66c6f
MJ Liu <MJ.Liu@silabs.com>
2021-06-05 18:39:00 -0400

    Complete "Extend User Project" feature

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {
   Divider,
   Typography,
-  Button
+  Button,
+  Card,
+  CardMedia,
+  CardActionArea
 } from '@material-ui/core';
 import './userDetail.css';
 import { Link } from 'react-router-dom';
@@ -13,10 +16,13 @@ import PhotoIcon from '@material-ui/icons/Photo';
  * Define UserDetail, a React component of CS142 project #5
  */
 class UserDetail extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       curUser: "",
+      recentPhotoName: "",
+      commentPhotoName: "",
     };
     let userId = props.match.params.userId;
     axios.get("/user/" + userId)
@@ -24,6 +30,21 @@ class UserDetail extends React.Component {
         let user = response.data;
         this.setState({ curUser: user });
         this.props.switchPage("Profile of " + user.first_name + " " + user.last_name);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      })
+
+    axios.get("/photosOfUser/" + userId)
+      .then((response) => {
+        let photos = response.data;
+        if (photos && photos.length !== 0) {
+          photos.sort(this.compareRecentPhotos);
+          this.setState({ recentPhotoName: photos[0].file_name });
+
+          photos.sort(this.compareCommentPhotos);
+          this.setState({ commentPhotoName: photos[0].file_name });
+        }
       })
       .catch((error) => {
         console.log(error.response);
@@ -42,7 +63,30 @@ class UserDetail extends React.Component {
       .catch((error) => {
         console.log(error.response);
       })
+
+      axios.get("/photosOfUser/" + userId)
+      .then((response) => {
+        let photos = response.data;
+        if (photos && photos.length !== 0) {
+          photos.sort(this.compareRecentPhotos);
+          this.setState({ recentPhotoName: photos[0].file_name });
+
+          photos.sort(this.compareCommentPhotos);
+          this.setState({ commentPhotoName: photos[0].file_name });
+        }
+      })
+      .catch((error) => {
+        console.log(error.response);
+      })
     }
+  }
+
+  compareRecentPhotos(photoA, photoB) {
+    return (photoA.date_time < photoB.date_time ? 1 : -1);
+  }
+
+  compareCommentPhotos(photoA, photoB) {
+    return (photoA.comments.length < photoB.comments.length ? 1 : -1);
   }
 
   render() {
@@ -69,6 +113,36 @@ class UserDetail extends React.Component {
         > 
           View Photos
         </Button>
+        <div className="cs142-details-extend">
+          <Typography variant="body1" gutterBottom> <b>Your Most Recently Uploaded Photo:</b></Typography>
+          {this.state.recentPhotoName && (
+            <Card className="cs142-details-thumbnail">
+              <CardActionArea
+                component={Link}
+                to={"/photos/" + user._id}
+              >
+              <CardMedia
+                component="img"
+                image={"/images/" + this.state.recentPhotoName}
+              />
+              </CardActionArea>
+            </Card>
+          )}
+          <Typography variant="body1" gutterBottom> <b>Your Photo with Most Comments:</b></Typography>
+          {this.state.commentPhotoName && (
+            <Card className="cs142-details-thumbnail">
+              <CardActionArea
+                component={Link}
+                to={"/photos/" + user._id}
+              >
+              <CardMedia
+                component="img"
+                image={"/images/" + this.state.commentPhotoName}
+              />
+              </CardActionArea>
+            </Card>
+          )}
+        </div>
       </div>
     );
   }
